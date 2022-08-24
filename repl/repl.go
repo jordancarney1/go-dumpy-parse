c4a84e10a4bb0bbc91eb4e802fb59547e60de7b7
jordan <jcarney@bluegracegroup.com>
2022-08-24 13:06:36 -0400

    REPL fun.

--- repl/repl.go
+++ repl/repl.go
@@ -3,11 +3,25 @@ package repl
 import (
 	"bufio"
 	"dumpy/lexer"
-	"dumpy/token"
+	"dumpy/parser"
 	"fmt"
 	"io"
 )
 
+// TODO: Replace with ascii dumpster art
+const DUMPSTER = `
+$$$$$$$\                                              
+$$  __$$\                                             
+$$ |  $$ |$$\   $$\ $$$$$$\$$$$\   $$$$$$\  $$\   $$\ 
+$$ |  $$ |$$ |  $$ |$$  _$$  _$$\ $$  __$$\ $$ |  $$ |
+$$ |  $$ |$$ |  $$ |$$ / $$ / $$ |$$ /  $$ |$$ |  $$ |
+$$ |  $$ |$$ |  $$ |$$ | $$ | $$ |$$ |  $$ |$$ |  $$ |
+$$$$$$$  |\$$$$$$  |$$ | $$ | $$ |$$$$$$$  |\$$$$$$$ |
+\_______/  \______/ \__| \__| \__|$$  ____/  \____$$ |
+                                  $$ |      $$\   $$ |
+                                  $$ |      \$$$$$$  |
+                                  \__|       \______/ 
+`
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
@@ -22,9 +36,30 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+
+		// for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+		// 	fmt.Fprintf(out, "%+v\n", tok)
+		// }
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, DUMPSTER)
+	io.WriteString(out, "Woops! We ran into some garbage here!\n")
+	io.WriteString(out, " parser errors:\n")
+
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
